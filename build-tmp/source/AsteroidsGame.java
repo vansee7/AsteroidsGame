0d0f1539d3fe7de856af98d20b4f3e4789269ff5
unknown <vasee@697-334-Bard.sf.k12.internal>
2014-11-10 10:24:29 -0800

    Created spaceship and stars.

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 SpaceShip Earth;
 Star [] galaxy;
 
@@ -165,4 +181,13 @@ public void keyPressed()
    {
     Earth.setPointDirection((int)Earth.myPointDirection+5);
    }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
